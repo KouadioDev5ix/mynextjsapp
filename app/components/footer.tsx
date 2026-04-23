7a293e8c29d08dac5afbbc620a5cb419441d3922
Kouadio Alfred <alfred.justbelieve.5@gmail.com>
2026-04-23 15:48:24 +0000

    Update Footer component height and margin for improved layout

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <section className="w-full mainBg h-40 ">
+    <section className="w-full mainBg h-72 mt-10">
       <div className="flex items-center justify-center pt-10">
         <p className="font-bold text-gray-100">2026 @ - All rigth reserves</p>
       </div>

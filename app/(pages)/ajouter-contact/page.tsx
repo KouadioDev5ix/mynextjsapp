d6c730899f962db311224304f33bfd1d8027f338
Kouadio Alfred <alfred.justbelieve.5@gmail.com>
2026-04-23 14:19:43 +0000

    Refactor form structure in addContactForms component

--- app/(pages)/ajouter-contact/page.tsx
+++ app/(pages)/ajouter-contact/page.tsx
@@ -1,11 +1,7 @@
 export default function addContactForms() {
   return (
-    <form
-      action="
-    
-    "
-      className="pt-28"
-    >
+    <form action="" className="pt-28">
+      <h1 className="text-2xl font-extrabold text-stone-950">Ajouter un prestataire</h1>
       <p>C'esst la page d'ajout de contact</p>
     </form>
   );

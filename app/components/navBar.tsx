42e7f5ed1685c8d5db38e9f5a7b4fbedf1ca94a6
Kouadio Alfred <alfred.justbelieve.5@gmail.com>
2026-04-23 11:14:21 +0000

    Add NavBar component for user management interface

---
+++ app/components/navBar.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+export const NavBar = () => {
+
+  return(
+ <div className="w-full mainBg fixed top-0 drop-shadow-sm z-50">
+    <div className="w-10/12 max-w-7xl mx-auto bg-mainBg px-4 h-20 text-white flex items-center justify-between">
+      <div>
+        <h1 className="text-2xl font-bold">UserManager</h1>
+      </div>
+      <ul>
+        <li className="font-bold">Ajouter un utilisateur</li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  </div>
+
+  )
+
+ 
+};

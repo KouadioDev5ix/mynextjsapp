1f0223cfd76196168e383ac0f261915f0a13a22b
Kouadio Alfred <alfred.justbelieve.5@gmail.com>
2026-04-23 15:48:07 +0000

    Refactor AddContactForms component structure and integrate form handling

--- app/(pages)/ajouter-contact/page.tsx
+++ app/(pages)/ajouter-contact/page.tsx
@@ -1,8 +1,36 @@
-export default function addContactForms() {
+"use client";
+
+import { IPrestataireFormData } from "@/interfaces/prestataires";
+import { prestataireSchema } from "@/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+export default function AddContactForms() {
+  const {
+    register,
+    control,
+    formState: { errors },
+  } = useForm<IPrestataireFormData>({
+    resolver: zodResolver(prestataireSchema),
+  });
+
   return (
-    <form action="" className="pt-28">
-      <h1 className="text-2xl font-extrabold text-stone-950">Ajouter un prestataire</h1>
-      <p>C'esst la page d'ajout de contact</p>
-    </form>
+    <section className="pt-28">
+      <div className="flex-col flex items-center justify-center pb-5">
+        <h1 className="text-2xl font-extrabold text-stone-800 ">
+          Ajouter un prestataire
+        </h1>
+      </div>
+
+      <section className="max-w-3xl mx-auto px-5 border bg-white border-gray-200 shadow-xs rounded-xl h-[800px] pb-10">
+        <form action="" className="">
+          <div></div>
+
+          <div className="">
+            <div>{/* <p>C'esst la page d'ajout de contact</p> */}</div>
+          </div>
+        </form>
+      </section>
+    </section>
   );
 }

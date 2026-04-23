7c14c036299846ca12bd6fc68b8dcdc93248318c
Kouadio Alfred <alfred.justbelieve.5@gmail.com>
2026-04-23 11:14:13 +0000

    Add Footer component with copyright notice and styling

---
+++ app/components/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <section className="w-full mainBg h-40 ">
+      <div className="flex items-center justify-center pt-10">
+        <p className="font-bold text-gray-100">2026 @ - All rigth reserves</p>
+      </div>
+    </section>
+  );
+}

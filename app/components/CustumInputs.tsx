6e25bd7860bb1b14da1180fe3d138bb2520aafd6
Kouadio Alfred <alfred.justbelieve.5@gmail.com>
2026-04-24 23:22:27 +0000

    Refactor CustumInputs component layout and ensure input field is full width

--- app/components/CustumInputs.tsx
+++ app/components/CustumInputs.tsx
@@ -25,9 +25,11 @@ export default function CustumInputs({
 }: CustumInputs) {
   return (
     <div>
-      <label htmlFor={htmlFor} className={labelClassName}>
-        {label}
-      </label>
+      <div>
+        <label htmlFor={htmlFor} className={labelClassName}>
+          {label}
+        </label>
+      </div>
 
       <input
         type={inputType}
@@ -35,7 +37,7 @@ export default function CustumInputs({
         id={id}
         placeholder={placeHolder}
         className={cn(
-          "border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-100 outline-none rounded-lg px-3 py-2",
+          "w-full border border-gray-300 focus:border-blue-500 focus:ring-2 focus:ring-blue-100 outline-none rounded-lg px-3 py-2",
           inputClassName,
         )}
         {...rest}

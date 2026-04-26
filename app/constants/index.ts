d3f065aaf7db3480bf3b8927ce27ab87ffd62617
Kouadio Alfred <alfred.justbelieve.5@gmail.com>
2026-04-26 21:21:33 +0000

    Add genreOptions constant to index.ts

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,3 +1,5 @@
-export const  genreOptions = [
-  
-]
+export const genreOptions = [
+  { id: "Homme", label: "Homme" },
+  { id: "Femme", label: "Femme" },
+  { id: "Non précisé", label: "Non précisé" },
+];

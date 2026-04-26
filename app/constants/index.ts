57820141d80dbc65b00cea8966a953b36d6d45cd
Kouadio Alfred <alfred.justbelieve.5@gmail.com>
2026-04-26 21:23:48 +0000

    Add paysOptions constant to index.ts

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -12,3 +12,12 @@ export const nationaliteOptions = [
   { id: "Malienne", label: "Malienne" },
   { id: "Autre", label: "Autre" },
 ];
+
+const paysOptions = [
+  { id: "Côte d'Ivoire", label: "Côte d'Ivoire" },
+  { id: "France", label: "France" },
+  { id: "Sénégal", label: "Sénégal" },
+  { id: "Burkina Faso", label: "Burkina Faso" },
+  { id: "Mali", label: "Mali" },
+  { id: "Autre", label: "Autre" },
+];
